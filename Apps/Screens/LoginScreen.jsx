c84462cf86d1a167b52d8aa7fa9d7630beb9e332
shallon <shallonmokeira@gmail.com>
2024-04-01 21:30:37 +0300

    add clickable effect

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
@@ -11,9 +11,9 @@ export default function LoginScreen() {
        <View className="p-8">
         <Text className="text-[35px] font-bold">Utibu Pharmacy</Text>
         <Text className="text-[18px] text-slate-500 mt-6" >Never run out of your essential medications again. Easily refill your prescriptions through our app, and we'll ensure your medications are ready for pickup or delivery.</Text>
-        <View className="p-4 bg-blue-500 rounded-full mt-20">
+        <TouchableOpacity className="p-4 bg-blue-500 rounded-full mt-20">
           <Text className="text-white text-center text-[18px]">Get Started</Text>
-        </View>
+        </TouchableOpacity>
 
        </View>
     </View>
