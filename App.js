e67b70d2fcb3671c8e03ccd8a100f2d4068db8e2
shallon <shallonmokeira@gmail.com>
2024-04-01 22:12:07 +0300

    add the publisher key

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import LoginScreen from './Apps/Screens/LoginScreen';
 import { ClerkProvider } from '@clerk/clerk-expo';
 export default function App() {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey='pk_test_YWRhcHRlZC1jYXJkaW5hbC0zNy5jbGVyay5hY2NvdW50cy5kZXYk'>
     <View className="flex-1  bg-white">
       <StatusBar style="auto" />
       <LoginScreen />
