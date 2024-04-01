9771d409c0bde101892721f2fe1c7feb5338ecd9
shallon <shallonmokeira@gmail.com>
2024-04-01 20:13:03 +0300

    display login screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Apps/Screens/LoginScreen';
 
 export default function App() {
   return (
     <View className="flex-1  bg-white">
       <StatusBar style="auto" />
+      <LoginScreen />
     </View>
   );
 }
