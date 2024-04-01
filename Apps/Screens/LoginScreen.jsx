d87eeb34d5d74e8128451da01fbe08cdb5208ecb
shallon <shallonmokeira@gmail.com>
2024-04-01 21:39:52 +0300

    modify screen styling

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -8,7 +8,7 @@ export default function LoginScreen() {
            className="w-full h-[400px] object-cover"
        
        />
-       <View className="p-8">
+       <View className="p-8 bg-white mt-[-20px] rounded-t-3xl shadow-md">
         <Text className="text-[35px] font-bold">Utibu Pharmacy</Text>
         <Text className="text-[18px] text-slate-500 mt-6" >Never run out of your essential medications again. Easily refill your prescriptions through our app, and we'll ensure your medications are ready for pickup or delivery.</Text>
         <TouchableOpacity onPress={()=>console.log("SignIn")} className="p-4 bg-blue-500 rounded-full mt-20">
