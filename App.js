d36f09a80bccd0967fe5d7ddd9cd80bc938bfc1d
shallon <shallonmokeira@gmail.com>
2024-04-01 22:09:31 +0300

    mount clerk provider

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './Apps/Screens/LoginScreen';
-
+import { ClerkProvider } from '@clerk/clerk-expo';
 export default function App() {
   return (
+    <ClerkProvider>
     <View className="flex-1  bg-white">
       <StatusBar style="auto" />
       <LoginScreen />
     </View>
+    </ClerkProvider>
   );
 }
 
