1ff53f4cb0904dbe14ced627b8c0cb25016aec26
shallon <shallonmokeira@gmail.com>
2024-04-01 21:34:23 +0300

    modify button

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -11,7 +11,7 @@ export default function LoginScreen() {
        <View className="p-8">
         <Text className="text-[35px] font-bold">Utibu Pharmacy</Text>
         <Text className="text-[18px] text-slate-500 mt-6" >Never run out of your essential medications again. Easily refill your prescriptions through our app, and we'll ensure your medications are ready for pickup or delivery.</Text>
-        <TouchableOpacity className="p-4 bg-blue-500 rounded-full mt-20">
+        <TouchableOpacity onPress={()=>console.log("SignIn")} className="p-4 bg-blue-500 rounded-full mt-20">
           <Text className="text-white text-center text-[18px]">Get Started</Text>
         </TouchableOpacity>
 
