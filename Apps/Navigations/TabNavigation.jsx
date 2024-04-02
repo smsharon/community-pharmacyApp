986ea0d8130f33e59429cd9fbaa9823a6052aad5
shallon <shallonmokeira@gmail.com>
2024-04-02 17:33:19 +0300

    add the screens component

--- Apps/Navigations/TabNavigation.jsx
+++ Apps/Navigations/TabNavigation.jsx
@@ -1,13 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../Screens/HomeScreen';
+import MedOrders from '../Screens/MedOrders';
+import MedRefills from '../Screens/MedRefills';
+import ProfileScreen from '../Screens/ProfileScreen';
 
 
 const Tab = createBottomTabNavigator();
 export default function TabNavigation() {
   return (
-    <View>
-      <Text>TabNavigation</Text>
-    </View>
+    <Tab.Navigator>
+        <Tab.Screen name='home' component={HomeScreen} />
+        <Tab.Screen name='orders' component={MedOrders} />
+        <Tab.Screen name='refills' component={MedRefills} />
+        <Tab.Screen name='profile' component={ProfileScreen} />
+    </Tab.Navigator>
   )
 }
