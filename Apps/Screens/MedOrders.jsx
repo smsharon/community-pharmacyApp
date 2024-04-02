7126b0b83e1a8bf0a2bcf7caa4c30bf038acad87
shallon <shallonmokeira@gmail.com>
2024-04-02 17:21:45 +0300

    add tabs files

---
+++ Apps/Screens/MedOrders.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MedOrders() {
+  return (
+    <View>
+      <Text>MedOrders</Text>
+    </View>
+  )
+}
