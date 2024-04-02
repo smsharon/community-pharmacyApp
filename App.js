a9190d2dc92064cc187ed0987a502a7d35a0f0c9
shallon <shallonmokeira@gmail.com>
2024-04-02 23:04:29 +0300

    modify the navigation tabs

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
     <ClerkProvider publishableKey='pk_test_YWRhcHRlZC1jYXJkaW5hbC0zNy5jbGVyay5hY2NvdW50cy5kZXYk'>
     <View className="flex-1  bg-white">
       <StatusBar style="auto" />
-      
+      <LoginScreen />
       <SignedIn>
           <NavigationContainer>
             <TabNavigation />
