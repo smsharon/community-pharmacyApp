b7d573d69eec8025ad78d2d8938588a889f81aa4
shallon <shallonmokeira@gmail.com>
2024-04-02 11:25:42 +0300

    implement and call the onpress method

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -11,6 +11,22 @@ export default function LoginScreen() {
   useWarmUpBrowser();
  
   const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+ 
   
   return (
     <View>
@@ -21,7 +37,7 @@ export default function LoginScreen() {
        <View className="p-8 bg-white mt-[-20px] rounded-t-3xl shadow-md">
         <Text className="text-[35px] font-bold">Utibu Pharmacy</Text>
         <Text className="text-[18px] text-slate-500 mt-6" >Never run out of your essential medications again. Easily refill your prescriptions through our app, and we'll ensure your medications are ready for pickup or delivery.</Text>
-        <TouchableOpacity onPress={()=>console.log("SignIn")} className="p-4 bg-blue-500 rounded-full mt-20">
+        <TouchableOpacity onPress={onPress} className="p-4 bg-blue-500 rounded-full mt-20">
           <Text className="text-white text-center text-[18px]">Get Started</Text>
         </TouchableOpacity>
 
