71316eb6f6f61ee740a9ffa115db716121e93e29
shallon <shallonmokeira@gmail.com>
2024-04-02 21:40:48 +0300

    initialize firestore

--- Apps/Screens/MedRefills.jsx
+++ Apps/Screens/MedRefills.jsx
@@ -1,7 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { app } from '../../firebaseConfig';
+import { getFirestore } from "firebase/firestore";
+
 
 export default function MedRefills() {
+
+    const db = getFirestore(app);
+    const getCategoryList=()=>{
+
+    }
   return (
     <View>
       <Text>MedRefills</Text>
