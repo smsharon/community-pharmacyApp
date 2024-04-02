a3e093f3eabf90c3eac8156e8ffd86563bb72ac1
shallon <shallonmokeira@gmail.com>
2024-04-02 18:19:51 +0300

    add proper label

--- Apps/Navigations/TabNavigation.jsx
+++ Apps/Navigations/TabNavigation.jsx
@@ -5,6 +5,7 @@ import HomeScreen from '../Screens/HomeScreen';
 import MedOrders from '../Screens/MedOrders';
 import MedRefills from '../Screens/MedRefills';
 import ProfileScreen from '../Screens/ProfileScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const Tab = createBottomTabNavigator();
@@ -13,10 +14,49 @@ export default function TabNavigation() {
     <Tab.Navigator screenOptions={{
         headerShown:false
     }}>
-        <Tab.Screen name='home' component={HomeScreen} />
-        <Tab.Screen name='orders' component={MedOrders} />
-        <Tab.Screen name='refills' component={MedRefills} />
-        <Tab.Screen name='profile' component={ProfileScreen} />
+        <Tab.Screen name='home' component={HomeScreen}
+           options={{
+            tabBarLabel:({color})=>(
+                <Text style={{color:color,fontSize:12,marginBottom:3}}>Home</Text>
+            ),
+            tabBarIcon:({color,size})=>(
+                <Ionicons name="home" size={size} color={color} />
+
+            )
+           }}
+        />
+        <Tab.Screen name='orders' component={MedOrders}
+        options={{
+            tabBarLabel:({color})=>(
+                <Text style={{color:color,fontSize:12,marginBottom:3}}>Medication Orders</Text>
+            ),
+            tabBarIcon:({color,size})=>(
+                <Ionicons name="receipt" size={size} color={color} />
+            )
+           }}
+        />
+        <Tab.Screen name='refills' component={MedRefills} 
+        options={{
+            tabBarLabel:({color})=>(
+                <Text style={{color:color,fontSize:12,marginBottom:3}}>Medication refills</Text>
+            ),
+            tabBarIcon:({color,size})=>(
+                <Ionicons name="medkit" size={size} color={color} />
+
+            )
+           }}
+        />
+        <Tab.Screen name='profile' component={ProfileScreen} 
+        options={{
+            tabBarLabel:({color})=>(
+                <Text style={{color:color,fontSize:12,marginBottom:3}}>Profile</Text>
+            ),
+            tabBarIcon:({color,size})=>(
+                <Ionicons name="person-circle" size={size} color={color} />
+
+            )
+           }}
+        />
     </Tab.Navigator>
   )
 }
