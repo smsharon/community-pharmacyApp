5e03b2783d66b903ea062500970bb92ca2ea7f32
shallon <shallonmokeira@gmail.com>
2024-04-03 14:49:47 +0300

    include profile image

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
 
 export default function ProfileScreen() {
+    const {user}=useUser();
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View className="p-5">
+      <Image source={{uri:user?.imageUrl}}
+        className="w-[100px] h-[100px] rounded-full items-center justify-center"
+      />
     </View>
   )
 }
