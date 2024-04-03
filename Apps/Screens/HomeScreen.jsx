df79f320a11f5b6845c2187f08b527dee9260512
shallon <shallonmokeira@gmail.com>
2024-04-03 22:11:38 +0300

    display category data on homescreen

--- Apps/Screens/HomeScreen.jsx
+++ Apps/Screens/HomeScreen.jsx
@@ -5,12 +5,15 @@ import Slider from '../Components/HomeScreen/Slider'
 import { getFirestore } from "firebase/firestore";
 import { app } from '../../firebaseConfig';
 import { collection, getDocs } from "firebase/firestore";
+import Categories from '../Components/HomeScreen/Categories';
 
 export default function HomeScreen() {
     const db = getFirestore(app);
     const [sliderList, setSliderList]=useState([]);
+    const [categoryList, setCategoryList]=useState([]);
     useEffect(()=>{
       getSliders();
+      getCategoryList();
 
     },[])
     /**used to get sliders for home screen */
@@ -25,11 +28,25 @@ querySnapshot.forEach((doc) => {
 
 
     }
+    /**used to gcategory list for home screen */
+    const getCategoryList=async()=>{
+      setCategoryList([])
+    const querySnapshot = await getDocs(collection(db, "Categories"));
+querySnapshot.forEach((doc) => {
+  console.log("Docs:",doc.data());
+  setCategoryList(CategoryList=>[...CategoryList,doc.data()]);
+});
+    }
+
+
+
   return (
     <View className="py-8 px-6 bg-white flex-1">
       <Header />
       {/*slider */}
       <Slider sliderList={sliderList} />
+      {/*categoryList*/}
+      <Categories categoryList={categoryList} />
     </View>
   )
 }
