06e231b906070b2abd390a28ca87d4cd4a875882
shallon <shallonmokeira@gmail.com>
2024-04-03 17:11:14 +0300

    initialize firestore

--- Apps/Screens/HomeScreen.jsx
+++ Apps/Screens/HomeScreen.jsx
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../Components/Header'
+import Slider from '../Components/HomeScreen/Slider'
+import { getFirestore } from "firebase/firestore";
+import { app } from '../../firebaseConfig';
 
 export default function HomeScreen() {
+    const db = getFirestore(app);
+    const getSliders=()=>{
+
+    }
   return (
     <View className="py-8 px-6 bg-white flex-1">
       <Header />
+      {/*slider */}
+      <Slider />
     </View>
   )
 }
