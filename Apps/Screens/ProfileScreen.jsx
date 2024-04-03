bbf2d93b8a95395c63ae1e0e31ee663bed7bb8b9
shallon <shallonmokeira@gmail.com>
2024-04-03 16:37:57 +0300

    add tabs and style

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-expo'
 import orders from './../../assets/images/orders.jpg'
@@ -15,12 +15,12 @@ export default function ProfileScreen() {
             icon: orders,
         },
         {
-            id:1,
+            id:2,
             name: 'My Refills',
             icon: refills,
         },
         {
-            id:1,
+            id:3,
             name: 'Logout',
             icon: logout,
         }
@@ -35,7 +35,21 @@ export default function ProfileScreen() {
       <Text className="text-[18x] mt-2 text-gray-500">{user?.primaryEmailAddress?.emailAddress}</Text>
 
         </View>
+        <FlatList
+        data={menuList}
+        numColumns={2}
+        style={{marginTop:20}}
+        renderItem={({item,index})=>(
+            <TouchableOpacity className="flex-1 p-3 border-[1px] items-center mx-2 mt-4 rounded-lg border-blue-400 bg-blue-50">
+                {item.icon&& <Image source={item?.icon}
+                className="w-[50px] h-[50px]"
+                />}
+                <Text className="text-[12px] mt-2 text-blue-700">{item.name}</Text>
+            </TouchableOpacity>
+        )}
+        />
       
     </View>
+
   )
 }
