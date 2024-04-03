c5b7bc54fe4168cacd9055a13e77da564206ba92
shallon <shallonmokeira@gmail.com>
2024-04-03 16:10:45 +0300

    add image urls

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -1,9 +1,30 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-expo'
+import orders from './../../assets/images/orders.jpg'
+import refills from './../../assets/images/refills.jpg'
+import logout from './../../assets/images/logout.png'
 
 export default function ProfileScreen() {
     const {user}=useUser();
+
+    const menuList=[
+        {
+            id:1,
+            name: 'My Orders',
+            icon: orders,
+        },
+        {
+            id:1,
+            name: 'My Refills',
+            icon: refills,
+        },
+        {
+            id:1,
+            name: 'Logout',
+            icon: logout,
+        }
+    ]
   return (
     <View className="p-5">
         <View className="items-center mt-14">
