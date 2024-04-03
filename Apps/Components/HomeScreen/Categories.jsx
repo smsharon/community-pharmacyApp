aef8ebc3207f06fa8fc36e8796ed2ddc9186ba59
shallon <shallonmokeira@gmail.com>
2024-04-03 22:11:06 +0300

    fetch categories data from firestore

--- Apps/Components/HomeScreen/Categories.jsx
+++ Apps/Components/HomeScreen/Categories.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
 import React from 'react'
 
-export default function Categories() {
+export default function Categories({categoryList}) {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View className="mt-3">
+      <Text className="font-bold text-[20px]">Popular medicine</Text>
+      <FlatList 
+      data={categoryList}
+      numColumns={3}
+      renderItem={({item,index})=>(
+        <View className="flex-1 items-center justify-center p-4 border-[1px] border-gray-300 m-1 h-[250px] rounded-lg">
+          <Image source={{uri:item?.image}}
+          className="w-[100px] h-[200px]"
+          />
+          <Text>{item.name}</Text>
+        </View>
+      )}
+      />
     </View>
   )
 }
