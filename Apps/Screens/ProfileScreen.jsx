186a87c43025f081269c6d07f625ecb6c3b47d79
shallon <shallonmokeira@gmail.com>
2024-04-03 14:59:00 +0300

    display profile details

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -6,9 +6,15 @@ export default function ProfileScreen() {
     const {user}=useUser();
   return (
     <View className="p-5">
-      <Image source={{uri:user?.imageUrl}}
-        className="w-[100px] h-[100px] rounded-full items-center justify-center"
+        <View className="items-center mt-14">
+        <Image source={{uri:user?.imageUrl}}
+        className="w-[100px] h-[100px] rounded-full"
       />
+      <Text className="font-bold text-[25px] mt-2">{user?.fullName}</Text>
+      <Text className="text-[18x] mt-2 text-gray-500">{user?.primaryEmailAddress?.emailAddress}</Text>
+
+        </View>
+      
     </View>
   )
 }
