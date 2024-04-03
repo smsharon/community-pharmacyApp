da47193c61e8afedc75cdbc79e41e62c0f871d7e
shallon <shallonmokeira@gmail.com>
2024-04-04 01:17:00 +0300

    post orders to firestore

--- Apps/Screens/MedRefills.jsx
+++ Apps/Screens/MedRefills.jsx
@@ -1,7 +1,8 @@
-import { View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig';
 import { getFirestore,getDocs,collection } from "firebase/firestore";
+import { addDoc } from 'firebase/firestore';
 
 
 export default function MedRefills() {
@@ -26,6 +27,35 @@ export default function MedRefills() {
       });
       setMedicineList(medicines);
   }
+  const refillMedication = async (medication) => {
+    try {
+      // Construct the order object with necessary details
+      const order = {
+          medication: medication.name,
+          quantity: 1, // For simplicity, assuming quantity is always 1
+          orderDate: new Date().toISOString(), // Current date and time
+          // Add more fields as needed
+      };
+
+      // Add the order document to the "orders" collection
+      const docRef = await addDoc(collection(db, 'orders'), order);
+      console.log('Order placed successfully with ID: ', docRef.id);
+    Alert.alert(
+        "Order Placed",
+        "You have successfully placed an order. Payment will be done on pickup.",
+        [
+            {
+                text: "OK",
+                onPress: () => console.log("Order placed alert closed"),
+            }
+        ]
+    );
+  } catch (error) {
+    console.error('Error placing order: ', error);
+    // Show an error message to the user
+    alert('Failed to place order. Please try again later.');
+}
+};
 
   const renderItem = ({ item }) => (
       <View style={{ flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ccc', paddingVertical: 10, paddingTop:4, borderRadius: 8, marginBottom: 10,padding: 10,}}>
