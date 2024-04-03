06f59fe3d403082f587bb79b47cb02b0abf5431c
shallon <shallonmokeira@gmail.com>
2024-04-03 23:59:10 +0300

    add search bar

--- Apps/Screens/MedRefills.jsx
+++ Apps/Screens/MedRefills.jsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig';
 import { getFirestore,getDocs,collection } from "firebase/firestore";
 
@@ -7,14 +7,17 @@ import { getFirestore,getDocs,collection } from "firebase/firestore";
 export default function MedRefills() {
 
     const db = getFirestore(app);
+    const [medicineList, setMedicineList]=useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(()=>{
-        getCategoryList();
+        getMedicineList();
 
     },[])
 
 
-    const getCategoryList=async()=>{
+    const getMedicineList=async()=>{
+      setMedicineList([])
         const querySnapshot=await getDocs(collection(db,'medicines'));
         querySnapshot.forEach((doc)=>{
             console.log("Docs:",doc.data());
@@ -24,6 +27,14 @@ export default function MedRefills() {
   return (
     <View className="p-5 py-12">
       <Text className="text-[30px] font-bold">Order your Medicines</Text>
+      <View style={{ marginTop: 20, marginBottom: 20 }}>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, padding: 10 }}
+                    onChangeText={setSearchQuery}
+                    value={searchQuery}
+                    placeholder="Search for a medication"
+                />
+            </View>
     </View>
   )
 }
