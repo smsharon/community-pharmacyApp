8e098e8fbdce9bc9dbb9946dfbd9f02c1daa82a8
shallon <shallonmokeira@gmail.com>
2024-04-04 02:38:45 +0300

    add navigation to the profile screen

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -4,20 +4,25 @@ import { useUser } from '@clerk/clerk-expo'
 import orders from './../../assets/images/orders.jpg'
 import refills from './../../assets/images/refills.jpg'
 import logout from './../../assets/images/logout.png'
+import { useNavigation } from '@react-navigation/native'
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function ProfileScreen() {
     const {user}=useUser();
-
+    const navigation = useNavigation();
+    const { isLoaded,signOut } = useAuth();
     const menuList=[
         {
             id:1,
             name: 'My Orders',
             icon: orders,
+            path: 'orders-tab'
         },
         {
             id:2,
             name: 'My Refills',
             icon: refills,
+            path: 'orders'
         },
         {
             id:3,
@@ -25,6 +30,16 @@ export default function ProfileScreen() {
             icon: logout,
         }
     ]
+
+    const onMenuPress=(item)=>{
+        if(item.name=='Logout')
+        {
+            signOut();
+            return;
+        }
+        item?.path?navigation.navigate(item.path):null;
+
+    }
   return (
     <View className="p-5">
         <View className="items-center mt-14">
@@ -40,7 +55,9 @@ export default function ProfileScreen() {
         numColumns={2}
         style={{marginTop:20}}
         renderItem={({item,index})=>(
-            <TouchableOpacity className="flex-1 p-3 border-[1px] items-center mx-2 mt-4 rounded-lg border-blue-400 bg-blue-50">
+            <TouchableOpacity
+            onPress={()=>onMenuPress(item)}
+            className="flex-1 p-3 border-[1px] items-center mx-2 mt-4 rounded-lg border-blue-400 bg-blue-50">
                 {item.icon&& <Image source={item?.icon}
                 className="w-[50px] h-[50px]"
                 />}
